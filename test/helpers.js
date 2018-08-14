8d2191bb1159b6a65ba49d4ad12fe12b491ea391
indexzero <charlie.robbins@gmail.com>
2018-08-14 12:40:50 -0700

    [dist test] Almost have tests.

--- test/helpers.js
+++ test/helpers.js
@@ -1,8 +1,48 @@
+const fs = require('fs');
+const path = require('path');
 const Repos = require('../repos');
 const Orgs = require('../orgs');
 
-exports.createRepos = function () {
+let githubAuth;
+const localAuthFile = path.join(__dirname, '..', '.localauth');
 
+exports.getGithubAuth = function () {
+  if (githubAuth) { return githubAuth; }
+
+  try {
+    githubAuth = fs.readFileSync(localAuthFile);
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
+
+  if (!githubAuth) {
+    const {
+      GITERATE_KEY,
+      GITERATE_APP_ID,
+      GITERATE_APP_SECRET
+    } = process.env;
+
+    if (GITERATE_KEY) {
+      githubAuth = { key: GITERATE_KEY };
+    } else if (GITERATE_APP_ID && GITERATE_APP_SECRET) {
+      githubAuth = {
+        app: {
+          id: GITERATE_APP_ID,
+          secret: GITERATE_APP_SECRET
+        }
+      };
+    } else {
+      throw new Error('No Github Credentials found.');
+    }
+  }
+
+  return githubAuth;
+}
+
+exports.createRepos = function () {
+  const auth = exports.getGithubAuth();
 };
 
 exports.createOrgs = function () {
--- test/repos/files.test.js
+++ test/repos/files.test.js
@@ -1,6 +1,12 @@
 const { createRepos } = require('../helpers');
 
 describe('.files()', function () {
+  let repos;
+
+  beforeEach(() => {
+    repos = createRepos();
+  })
+
   it('.forEach(fn)', function (done) {
     done();
   });
