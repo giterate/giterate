ac3a1b1a554f0daddabfbca49bcbcb504e9646b7
msluther <msluther99@yahoo.com>
2018-08-16 09:35:23 -0700

    read files & file contents from branches

--- repos/contents.js
+++ repos/contents.js
@@ -11,23 +11,28 @@ module.exports = class Contents extends ReaderWriter {
   async readCore() {
     const data = [];
     const files = await this.files.read();
-    for (const { repo, file } of files) {
+    for (const { repo, branch, file } of files) {
       let content = file.content;
       if (!content) {
-        const temp = await this.readSingle(repo, file);
+        const temp = await this.readSingle(repo, branch, file);
         content = temp.content;
       }
       content = Buffer.from(content || '', 'base64').toString();
-      data.push({ repo, file, content });
+      data.push({ repo, branch, file, content });
     }
 
     return data;
   }
 
   // TODO: Do this or just promisify all of githulk?
-  readSingle(repo, file) {
+  readSingle(repo, branch, file) {
     return new Promise((resolve, reject) => {
-      this.hulk.repository.contents(repo.full_name,  { path: file.path }, (err, results) => {
+      const options = { path: file.path };
+      if (branch && branch.name) {
+        options.ref = branch.name;
+      }
+
+      this.hulk.repository.contents(repo.full_name,  options, (err, results) => {
         if (err) {
           return void reject(err);
         }
--- repos/files.js
+++ repos/files.js
@@ -19,14 +19,14 @@ const Files = module.exports = class Files extends ReaderWriter {
     // Read all the parent repos data
     const repos = await this.repos.read();
     // TODO: simplify with async map.
-    for (const { repo } of repos) {
+    for (const { repo, branch } of repos) {
       // Get the data
-      let files = await this.readSingle(repo);
+      let files = await this.readSingle(repo, branch);
       if (this.filterFn) {
         files = files.filter(this.filterFn);
       }
       if (files && files.length) {
-        data.push.apply(data, files.map(file => ({ repo, file })));
+        data.push.apply(data, files.map(file => ({ repo, branch, file })));
       }
     }
 
@@ -34,9 +34,14 @@ const Files = module.exports = class Files extends ReaderWriter {
   }
 
   // TODO: Do this or just promisify all of githulk?
-  readSingle(repo) {
+  readSingle(repo, branch) {
     return new Promise((resolve, reject) => {
-      this.hulk.repository.contents(repo.full_name,  { path: this.path }, (err, results) => {
+      const options = { path: this.path };
+      if (branch && branch.name) {
+        options.ref = branch.name;
+      }
+
+      this.hulk.repository.contents(repo.full_name, options, (err, results) => {
         if (err) {
           return void reject(err);
         }
